86038e10ac02f738d740b91895f4819434e31cee
Jack Danger Canty <gitcommit@6brand.com>
2010-02-08 17:01:30 -0800

    fixing test indenting

--- test/four_info_test.rb
+++ test/four_info_test.rb
@@ -32,11 +32,14 @@ class FourInfoTest < ActiveSupport::TestCase
     end
     context "when phone number exists" do
       setup { @user.sms_phone_number = "206-555-5555"}
-      should "save confirmation number in proper attribute" do
-        assert @user.send(User.sms_confirmation_code_column)
-      end
-      should_change "stored code" do
-        @user.send User.sms_confirmation_code_column
+      context "confirming phone number" do
+        setup { @user.confirm_sms! }
+        should "save confirmation number in proper attribute" do
+          assert @user.send(User.sms_confirmation_code_column)
+        end
+        should_change "stored code" do
+          @user.send User.sms_confirmation_code_column
+        end
       end
     end
   end
