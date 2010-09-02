45986a57b0532fc0dd152f891046d388072d1231
Jack Danger Canty <gitcommit@6brand.com>
2010-09-01 22:35:52 -0700

    testing that users are validated by voice

--- test/twilio_contactable_controller_test.rb
+++ test/twilio_contactable_controller_test.rb
@@ -95,11 +95,20 @@ class TwilioContactableControllerTest < ActionController::TestCase
     end
     context "receiving digits from Twilio" do
       setup {
-        @receive_params = {"contactable_type"=>"User", "FromState"=>"WA", "Direction"=>"outbound-api", "CalledState"=>"WA", "ToState"=>"WA", "AccountSid"=>"ABCDEFG", "contactable_id"=>"1", "CallerCountry"=>"US", "CallerZip"=>"98077", "Caller"=>"{number}", "FromCity"=>"WOODINVILLE", "From"=>"{number}", "action"=>"receive_voice_confirmation", "CallerCity"=>"WOODINVILLE", "FromCountry"=>"US", "FromZip"=>"98077", "To"=>"+12069305710", "ToCity"=>"SEATTLE", "CallStatus"=>"in-progress", "controller"=>"twilio_contactable", "ToZip"=>"98188", "CalledZip"=>"98188", "CallerState"=>"WA", "Digits"=>"1234", "CalledCity"=>"SEATTLE", "Called"=>"+12069305710", "CalledCountry"=>"US", "CallSid"=>"ABCD", "ToCountry"=>"US", "ApiVersion"=>"2010-04-01"}
+        @digits = '12345'
+        @user = User.create! :phone_number => '206 555 6666',
+                             :voice_confirmation_code => @digits
+        @receive_params = {"contactable_type"=>"User", "contactable_id"=>@user.id, "Digits"=>@digits, "FromState"=>"WA", "Direction"=>"outbound-api", "CalledState"=>"WA", "ToState"=>"WA", "AccountSid"=>"ABCDEFG", "CallerCountry"=>"US", "CallerZip"=>"98077", "Caller"=>"{number}", "FromCity"=>"WOODINVILLE", "From"=>"{number}", "action"=>"receive_voice_confirmation", "CallerCity"=>"WOODINVILLE", "FromCountry"=>"US", "FromZip"=>"98077", "To"=>"+12069305710", "ToCity"=>"SEATTLE", "CallStatus"=>"in-progress", "controller"=>"twilio_contactable", "ToZip"=>"98188", "CalledZip"=>"98188", "CallerState"=>"WA", "CalledCity"=>"SEATTLE", "Called"=>"+12069305710", "CalledCountry"=>"US", "CallSid"=>"ABCD", "ToCountry"=>"US", "ApiVersion"=>"2010-04-01"}
         get :receive_voice_confirmation, @receive_params
       }
       should_respond_with :success
       should_respond_with_content_type :xml
+      should "set the user to confirmed via voice" do
+        assert @user.reload.voice_confirmed?
+      end
+      should "not change the user's sms confirmation setting" do
+        assert !@user.sms_confirmed?
+      end
     end
   end
 end
